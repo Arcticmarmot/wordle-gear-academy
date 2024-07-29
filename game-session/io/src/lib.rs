610e469d33682e39acbcdecadb0cec8a697e47b5
cybermarmot <cybermarmot1024@gmail.com>
2024-07-30 00:03:57 +0800

    complete the start game part

--- game-session/io/src/lib.rs
+++ game-session/io/src/lib.rs
@@ -1,13 +1,13 @@
 #![no_std]
 
 use gmeta::{InOut, Metadata};
-use gstd::{prelude::*, ActorId};
+use gstd::{prelude::*, ActorId, MessageId};
 
 pub struct GameSessionMetadata;
 
 impl Metadata for GameSessionMetadata {
     type Init = ();
-    type Handle = InOut<Action, Event>;
+    type Handle = InOut<SessionAction, SessionEvent>;
     type Others = ();
     type Reply = ();
     type Signal = ();
@@ -15,11 +15,64 @@ impl Metadata for GameSessionMetadata {
 }
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
-pub enum Action {
-
+pub enum SessionAction {
+    StartGame {
+        user: ActorId,
+    },
+    CheckWord {
+        user: ActorId,
+        word: String,
+    },
+    CheckGameStatus
 }
 
 #[derive(Debug, Clone, Encode, Decode, TypeInfo)]
-pub enum Event {
+pub enum SessionEvent {
+    UnStarted { info: String },
+    AlreadyStarted { info: String }
+}
 
+type SentMessageId = MessageId;
+type OriginalMessageId = MessageId;
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub enum SessionStatus {
+    #[default]
+    UnExisted,
+    GameStart{ 
+        user: ActorId
+    },
+    Gaming,
+    GameOver,
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct Session {
+    pub target_program_id: ActorId,
+    pub msg_ids: (SentMessageId, OriginalMessageId),
+    pub session_status: SessionStatus,
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum Action {
+    StartGame {
+        user: ActorId,
+    },
+    CheckWord {
+        user: ActorId,
+        word: String,
+    }
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+
+pub enum Event {
+    GameStarted {
+        user: ActorId,
+    },
+    WordChecked {
+        user: ActorId,
+        correct_positions: Vec<u8>,
+        contained_in_word: Vec<u8>,
+    },
 }
--- game-session/src/lib.rs
+++ game-session/src/lib.rs
@@ -1,19 +1,82 @@
 #![no_std]
+use game_session_io::*;
 use gstd::{exec, msg, prelude::*, ActorId};
 
+static mut SESSION: Option<Session> = None;
+const WAIT_BLOCKS: u8 = 3;
 #[no_mangle]
 extern "C" fn init() {
-    
+    let target_program_id = msg::load().expect("Unable to decode Init");
+    unsafe {
+        SESSION = Some(Session {
+            target_program_id,
+            msg_ids: (MessageId::zero(), MessageId::zero()),
+            session_status: SessionStatus::UnExisted,
+        });
+    }
 }
 
 #[no_mangle]
 extern "C" fn handle() {
+    let session = unsafe { SESSION.as_mut().expect("The session is not initialized") };
+    let action: SessionAction = msg::load().expect("Unable to decode `Action`");
     
+    match &session.session_status {
+        SessionStatus::UnExisted => {
+            match action {
+                Action::StartGame { user} => {
+                    let msg_id = msg::send(session.target_program_id, action, 0)
+                    .expect("Error in sending a message");
+                    session.msg_ids = (msg_id, msg::id());
+                    exec::wait();
+                }
+                _ => {
+                    msg::reply(SessionEvent::UnStarted { info: String::from("The game is unexisted") }, 0);
+                }
+            }
+        }
+        SessionStatus::GameStart { user: ActorId } => {
+            msg::send_delayed(exec::program_id(), SessionAction::CheckGameStatus, 0, WAIT_BLOCKS)
+                .expect("Error in sending a message");
+            msg::reply(SessionEvent::AlreadyStarted { info: String::from("The game is already started, and user_id is " + user)}, 0);
+            session.session_status = SessionStatus::Gaming;
+        }
+        SessionStatus::Gaming => {
+            match action {
+                Action::StartGame { user} => {
+                }
+                Action::CheckWord { user, word } => {
+
+                }
+                Action::CheckGameStatus => {
+
+                }
+            }
+        }
+        SessionStatus::GameOver => {
+
+        }
+        
+    }
 }
 
 #[no_mangle]
 extern "C" fn handle_reply() {
+    let reply_to = msg::reply_to().expect("Failed to query reply_to data");
+    let session = unsafe { SESSION.as_mut().expect("The session is not initialized") };
     
+    if reply_to == session.msg_ids.0 {
+        let reply_message: Event = msg::load().expect("Unable to decode `Event`");
+        match reply_message {
+            Event::GameStarted { user } => {
+                let original_message_id = session.msg_ids.1;
+                exec::wake(original_message_id).expect("Failed to wake message");
+            }
+            Event::WordChecked { user, correct_positions, contained_in_word } => {
+
+            }
+        }
+    }
 }
 
 #[no_mangle]
